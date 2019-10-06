e3d1732d1d706657250c75cdce50725779440145
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-06 17:27:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,7 +199,7 @@ def player_stats(players_name)
 end 
 
 def big_shoe_rebounds
-  result = [0]
+  result = 1
    game_hash.each do |place, team|
     team.each do |attribute, data|
        if attribute == :players
